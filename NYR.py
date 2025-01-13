570c6bccd76822cf019718911f5d1c03d372f0ce
salcxjo <salar.miri@gmail.com>
2025-01-13 14:50:13 -0700

    Updated main file to reflect plans for the project.

--- NYR.py
+++ NYR.py
@@ -1,1 +1,8 @@
-
+#Here is a main app where we will call functions and classes
+#from other .py files.
+#There will be a main page that loads with options
+#A function to set new goals
+#A function to edit goals
+#A function to contribute towards a goal (will have a summary progress report at end)
+#A function to show all contributions towards all goals (user can filter month, week, type of activity)
+#An option to see how much left till goal with that number chopped down to monthly, weekly and daily averages.
